105372aca354c3d36a4a241e0aa6ba3c67f68688
a5757657 <95263890+a5757657@users.noreply.github.com>
2022-11-23 19:56:51 +0800

    StageD

---
+++ src/views/StageD/D5.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const D5 = () => {
+  // 短衝檢視會議 Sprint Review 用來檢視該次短衝增量的成果， 以蒐集相關的回饋數據或意見。
+  return (
+    <div className='D5'>
+      <p className='redColor'>短衝檢視會議 Sprint Review</p>
+      <p className='mt'>用來檢視該次短衝增量的成果，</p>
+      <p>以蒐集相關的回饋數據或意見。</p>
+    </div>
+  )
+}
+
+export default D5
