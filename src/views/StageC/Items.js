a9dda19c0ed91b451dd28e80759012f393bd7391
Wei-Chi <winnie145ch@gmail.com>
2022-12-07 18:01:20 +0800

    trail

--- src/views/StageC/Items.js
+++ src/views/StageC/Items.js
@@ -1,47 +1,47 @@
-import React from 'react'
-import './Items.scss'
-
-const Items = (props) => {
-    const { content, point } = props
-    return (
-            <div className="item">
-                <div className="content">
-                    {content.map((item, index) => (
-                        <p key={item[0] + index}>{item}</p>
-                    ))}
-                </div>
-                <div className="point">{point}</div>
-            </div>
-    )
-}
-export default Items
-
 // import React from 'react'
-// import { Draggable } from 'react-beautiful-dnd'
 // import './Items.scss'
 
-// const Items = ({ item, index }) => {
+// const Items = (props) => {
+//     const { content, point } = props
 //     return (
-//         <Draggable draggabledId={item.id} index={index}>
-//             {(provided, snapshot) => {
-//                 return (
-//                     <div
-//                         className="item"
-//                         ref={provided.innerRef}
-//                         snapshot={snapshot}
-//                         {...provided.draggableProps}
-//                         {...provided.dragHandleProps}
-//                     >
-//                         <div className="content">
-//                             {item.content.map((e, i) => (
-//                                 <p key={e[0] + i}>{e}</p>
-//                             ))}
-//                         </div>
-//                         <div className="point">{item.point}</div>
-//                     </div>
-//                 )
-//             }}
-//         </Draggable>
+//             <div className="item">
+//                 <div className="content">
+//                     {content.map((item, index) => (
+//                         <p key={item[0] + index}>{item}</p>
+//                     ))}
+//                 </div>
+//                 <div className="point">{point}</div>
+//             </div>
 //     )
 // }
 // export default Items
+
+import React from 'react'
+import { Draggable } from 'react-beautiful-dnd'
+import './Items.scss'
+
+const Items = ({ item, index }) => {
+    return (
+        <Draggable draggabledId={item.id} index={index}>
+            {(provided, snapshot) => {
+                return (
+                    <div
+                        className="item"
+                        ref={provided.innerRef}
+                        snapshot={snapshot}
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                    >
+                        <div className="content">
+                            {item.content.map((e, i) => (
+                                <p key={e[0] + i}>{e}</p>
+                            ))}
+                        </div>
+                        <div className="point">{item.point}</div>
+                    </div>
+                )
+            }}
+        </Draggable>
+    )
+}
+export default Items
