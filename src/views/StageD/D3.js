3d40a6501d00a6e6bb9ed6a8ef9cc701587b9013
a5757657 <95263890+a5757657@users.noreply.github.com>
2023-03-21 15:10:08 +0800

    StageD

---
+++ src/views/StageD/D3.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const D3 = () => {
+  return (
+    <div className='D3'>
+      <p>除了每日站立會議</p>
+      <p>在 Sprint 結束後也會有：</p>
+      <p className='mt'>短衝檢視會議 SprintReview 與</p>
+      <p>短衝自省會議 Sprint Retrospective</p>
+    </div>
+  )
+}
+
+export default D3
--- src/views/StageD/StageD.js
+++ src/views/StageD/StageD.js
@@ -1,9 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './StageD.scss'
+import Bug from '../../components/Bug/Bug'
+import WhiteBlock from '../../components/WhiteBlock/WhiteBlock'
+import Button from '../../components/Button/Button'
+import D1 from './D1'
+import D2 from './D2'
+import D3 from './D3'
+import D4 from './D4'
+import D5 from './D5'
+import D6 from './D6'
+import D7 from './D7'
+
+const stageArr = ['D1', 'D2', 'D3', 'D4', 'D5', 'D6', 'D7']
 
 const StageD = () => {
+  const [nextStage, setNextStage] = useState(6)
+  const findStage = () => {
+    switch (stageArr[nextStage]) {
+      case 'D1':
+        return <D1 />
+      case 'D2':
+        return <D2 />
+      case 'D3':
+        return <D3 />
+      case 'D4':
+        return <D4 />
+      case 'D5':
+        return <D5 />
+      case 'D6':
+        return <D6 />
+      default:
+        break;
+    }
+  }
   return (
     <div className="StageD">
+      <Bug eyeColor='#0D0D0D' color='#D9D9D9' position={'leftTop'} />
+      {stageArr[nextStage] === 'D7' ? <D7 /> :
+        <WhiteBlock
+          button={
+            <Button
+              onClick={() => nextStage < 7 && setNextStage(nextStage + 1)}
+              color='#E14040'
+              arrow />
+          }
+        >
+        {findStage()}
+        </WhiteBlock>
+      }
     </div>
   )
 }
